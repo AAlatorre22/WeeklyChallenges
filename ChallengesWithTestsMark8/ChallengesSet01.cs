8b17262eddd8000c3114b1ae8dff7de0152e1532
Alejandro Alatorre <alejandro.c.alatorre@gmail.com>
2023-03-13 09:27:06 -0700

    Completed Week 1

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,56 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            //double sum = minuend - subtrahend;
+            //return sum;
+            return minuend - subtrahend;
+
+
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //int sum = number1 + number2;
+            //return sum;
+            return number1 + number2;
+            
+            //throw new NotImplementedException();
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return Math.Min(number1, number2);
+            
+            
+            //throw new NotImplementedException();
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
+           
+            
+            //throw new NotImplementedException();
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+
+            if (nameOfPerson == "")
+            {
+                return ("Hello!");
+            }
+            else return ($"Hello, {nameOfPerson}!");
+
+            //throw new NotImplementedException();
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return ("HEY!");
+           // throw new NotImplementedException();
         }
     }
 }
